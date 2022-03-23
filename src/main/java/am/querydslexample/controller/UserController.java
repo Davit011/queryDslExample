4b7f6d59dc8109e763fde38844c7abab627d60ca
Davit012 <jandavjan5252@mail.ru>
2022-03-23 19:15:32 +0400

    test git action

--- src/main/java/am/querydslexample/controller/UserController.java
+++ src/main/java/am/querydslexample/controller/UserController.java
@@ -1,7 +1,12 @@
 package am.querydslexample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @GetMapping("/user")
+    public String redirect()}
+sdgfd
+        barev dzez es uzum em stugel githubi quality control i ashxatanqy
 }
